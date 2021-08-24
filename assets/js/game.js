1ff0af4eab76be0b00d6267234306e9c1dbe2c77
andreaspencer <andreaaddawn@gmail.com>
2021-08-24 08:11:58 -0700

    player name entery fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -142,9 +142,21 @@ var shop = function() {
     }
 };
 
+var getPlayerName= function() {
+    var name = "";
+    //**************** */
+    //ADD LOOP HERE WITH PROMPT AND CONDITION
+    //**************** */
+    while (name === "" || name === null) {
+        name= prompt ("What is your robot's name?");
+    }
+
+    console.log("You robot's name is " + name);
+    return name;
+}
 //player info
 var playerInfo = {
-    name: window.prompt ("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
